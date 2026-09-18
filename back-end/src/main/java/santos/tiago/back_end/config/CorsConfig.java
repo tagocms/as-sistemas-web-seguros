44ef68a7db05d52d520c8aa107b4569dca1c1481
Tiago Camargo Maciel dos Santos <tagocms@gmail.com>
2026-09-17 21:42:42 -0300

    fix: corrected CorsConfig.java

--- back-end/src/main/java/santos/tiago/back_end/config/CorsConfig.java
+++ back-end/src/main/java/santos/tiago/back_end/config/CorsConfig.java
@@ -8,8 +8,9 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**")
-                .allowedOrigins("/localhost")
+        registry
+                .addMapping("/**")
+                .allowedOrigins("http://localhost:3001")
                 .allowedMethods("GET","POST", "PUT", "DELETE")
                 .allowedHeaders("*");
     }
